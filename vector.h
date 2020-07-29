267ee8b21cf2afbf853c983139d73a3a63404c0e
Avaer Kazmer <a@modules.io>
2020-07-29 04:37:20 -0400

    Update vector.{h,cc}

--- vector.h
+++ vector.h
@@ -5,6 +5,115 @@
 #include <vector>
 #include <algorithm>
 
+class Vec;
+class Quat;
+class Matrix;
+
+class Matrix {
+  public:
+    float elements[16];
+
+    Matrix() : elements() {}
+    
+    Matrix(const Matrix &m) {
+      for (unsigned int i = 0; i < sizeof(this->elements) / sizeof(this->elements[0]); i++) {
+        this->elements[i] = m.elements[i];
+      }
+    }
+
+    Matrix(float *elements) {
+      for (unsigned int i = 0; i < sizeof(this->elements) / sizeof(this->elements[0]); i++) {
+        this->elements[i] = elements[i];
+      }
+    }
+
+    Matrix(const Vec &position, const Quat &quaternion, const Vec &scale);
+
+    Matrix &operator*=(const Matrix &m) {
+      return this->multiply(*this, m);
+    }
+
+    Matrix operator*(const Matrix &m) const {
+      return Matrix().multiply(*this, m);
+    }
+
+    Matrix &multiply(const Matrix &a, const Matrix &b) {
+      const float * const ae = a.elements;
+      const float * const be = b.elements;
+      float *te = this->elements;
+
+      const float a11 = ae[ 0 ], a12 = ae[ 4 ], a13 = ae[ 8 ], a14 = ae[ 12 ];
+      const float a21 = ae[ 1 ], a22 = ae[ 5 ], a23 = ae[ 9 ], a24 = ae[ 13 ];
+      const float a31 = ae[ 2 ], a32 = ae[ 6 ], a33 = ae[ 10 ], a34 = ae[ 14 ];
+      const float a41 = ae[ 3 ], a42 = ae[ 7 ], a43 = ae[ 11 ], a44 = ae[ 15 ];
+
+      const float b11 = be[ 0 ], b12 = be[ 4 ], b13 = be[ 8 ], b14 = be[ 12 ];
+      const float b21 = be[ 1 ], b22 = be[ 5 ], b23 = be[ 9 ], b24 = be[ 13 ];
+      const float b31 = be[ 2 ], b32 = be[ 6 ], b33 = be[ 10 ], b34 = be[ 14 ];
+      const float b41 = be[ 3 ], b42 = be[ 7 ], b43 = be[ 11 ], b44 = be[ 15 ];
+
+      te[ 0 ] = a11 * b11 + a12 * b21 + a13 * b31 + a14 * b41;
+      te[ 4 ] = a11 * b12 + a12 * b22 + a13 * b32 + a14 * b42;
+      te[ 8 ] = a11 * b13 + a12 * b23 + a13 * b33 + a14 * b43;
+      te[ 12 ] = a11 * b14 + a12 * b24 + a13 * b34 + a14 * b44;
+
+      te[ 1 ] = a21 * b11 + a22 * b21 + a23 * b31 + a24 * b41;
+      te[ 5 ] = a21 * b12 + a22 * b22 + a23 * b32 + a24 * b42;
+      te[ 9 ] = a21 * b13 + a22 * b23 + a23 * b33 + a24 * b43;
+      te[ 13 ] = a21 * b14 + a22 * b24 + a23 * b34 + a24 * b44;
+
+      te[ 2 ] = a31 * b11 + a32 * b21 + a33 * b31 + a34 * b41;
+      te[ 6 ] = a31 * b12 + a32 * b22 + a33 * b32 + a34 * b42;
+      te[ 10 ] = a31 * b13 + a32 * b23 + a33 * b33 + a34 * b43;
+      te[ 14 ] = a31 * b14 + a32 * b24 + a33 * b34 + a34 * b44;
+
+      te[ 3 ] = a41 * b11 + a42 * b21 + a43 * b31 + a44 * b41;
+      te[ 7 ] = a41 * b12 + a42 * b22 + a43 * b32 + a44 * b42;
+      te[ 11 ] = a41 * b13 + a42 * b23 + a43 * b33 + a44 * b43;
+      te[ 15 ] = a41 * b14 + a42 * b24 + a43 * b34 + a44 * b44;
+
+      return *this;
+    }
+
+    static Matrix fromArray(float *elements) {
+      return Matrix(elements);
+    }
+};
+
+class Quat {
+  public:
+    union {
+      float data[4];
+      struct {
+        float x;
+        float y;
+        float z;
+        float w;
+      };
+    };
+
+    Quat() {
+      x = 0;
+      y = 0;
+      z = 0;
+      w = 1;
+    }
+
+    Quat(float ax, float ay, float az, float aw) {
+      x = ax;
+      y = ay;
+      z = az;
+      w = aw;
+    }
+
+    Quat(const Quat &q) {
+      x = q.x;
+      y = q.y;
+      z = q.z;
+      w = q.w;
+    }
+};
+
 // 3D Vector Class
 // Can also be used for 2D vectors
 // by ignoring the z value
@@ -187,9 +296,13 @@ class Vec {
           return (*this - v).magnitude_sqr();
         }
 
-        /* Vec &applyQuaternion(const Quat &q) {
-          float x = this->x, y = this->y, z = this->z;
-          float qx = q.x, qy = q.y, qz = q.z, qw = q.w;
+        float dot(const Vec &v) {
+          return this->x * v.x + this->y * v.y + this->z * v.z;
+        }
+
+        Vec &applyQuaternion(const Quat &q) {
+          const float x = this->x, y = this->y, z = this->z;
+          const float qx = q.x, qy = q.y, qz = q.z, qw = q.w;
 
           // calculate quat * vector
 
@@ -206,41 +319,23 @@ class Vec {
 
           return *this;
 
-        } */
-};
+        }
 
-class Quat {
-  public:
-    union {
-      float data[4];
-      struct {
-        float x;
-        float y;
-        float z;
-        float w;
-      };
-    };
+        Vec &applyMatrix(const Matrix &m) {
+          const float *e = m.elements;
 
-    Quat() {
-      x = 0;
-      y = 0;
-      z = 0;
-      w = 0;
-    }
+          const float w = 1.0f / ( e[ 3 ] * x + e[ 7 ] * y + e[ 11 ] * z + e[ 15 ] );
 
-    Quat(float ax, float ay, float az, float aw) {
-      x = ax;
-      y = ay;
-      z = az;
-      w = aw;
-    }
+          this->x = ( e[ 0 ] * x + e[ 4 ] * y + e[ 8 ] * z + e[ 12 ] ) * w;
+          this->y = ( e[ 1 ] * x + e[ 5 ] * y + e[ 9 ] * z + e[ 13 ] ) * w;
+          this->z = ( e[ 2 ] * x + e[ 6 ] * y + e[ 10 ] * z + e[ 14 ] ) * w;
 
-    Quat(const Quat &q) {
-      x = q.x;
-      y = q.y;
-      z = q.z;
-      w = q.w;
-    }
+          return *this;
+        }
+
+        Vec clone() const {
+          return Vec(x, y, z);
+        }
 };
 
 class Tri {
@@ -310,75 +405,6 @@ class Tri {
     }
 };
 
-class Matrix {
-  public:
-    float elements[16];
-
-    Matrix() : elements() {}
-    
-    Matrix(const Matrix &m) {
-      for (unsigned int i = 0; i < sizeof(this->elements) / sizeof(this->elements[0]); i++) {
-        this->elements[i] = m.elements[i];
-      }
-    }
-
-    Matrix(float *elements) {
-      for (unsigned int i = 0; i < sizeof(this->elements) / sizeof(this->elements[0]); i++) {
-        this->elements[i] = elements[i];
-      }
-    }
-
-    Matrix &operator*=(const Matrix &m) {
-      return this->multiply(*this, m);
-    }
-
-    Matrix operator*(const Matrix &m) const {
-      return Matrix().multiply(*this, m);
-    }
-
-    Matrix &multiply(const Matrix &a, const Matrix &b) {
-      const float * const ae = a.elements;
-      const float * const be = b.elements;
-      float *te = this->elements;
-
-      const float a11 = ae[ 0 ], a12 = ae[ 4 ], a13 = ae[ 8 ], a14 = ae[ 12 ];
-      const float a21 = ae[ 1 ], a22 = ae[ 5 ], a23 = ae[ 9 ], a24 = ae[ 13 ];
-      const float a31 = ae[ 2 ], a32 = ae[ 6 ], a33 = ae[ 10 ], a34 = ae[ 14 ];
-      const float a41 = ae[ 3 ], a42 = ae[ 7 ], a43 = ae[ 11 ], a44 = ae[ 15 ];
-
-      const float b11 = be[ 0 ], b12 = be[ 4 ], b13 = be[ 8 ], b14 = be[ 12 ];
-      const float b21 = be[ 1 ], b22 = be[ 5 ], b23 = be[ 9 ], b24 = be[ 13 ];
-      const float b31 = be[ 2 ], b32 = be[ 6 ], b33 = be[ 10 ], b34 = be[ 14 ];
-      const float b41 = be[ 3 ], b42 = be[ 7 ], b43 = be[ 11 ], b44 = be[ 15 ];
-
-      te[ 0 ] = a11 * b11 + a12 * b21 + a13 * b31 + a14 * b41;
-      te[ 4 ] = a11 * b12 + a12 * b22 + a13 * b32 + a14 * b42;
-      te[ 8 ] = a11 * b13 + a12 * b23 + a13 * b33 + a14 * b43;
-      te[ 12 ] = a11 * b14 + a12 * b24 + a13 * b34 + a14 * b44;
-
-      te[ 1 ] = a21 * b11 + a22 * b21 + a23 * b31 + a24 * b41;
-      te[ 5 ] = a21 * b12 + a22 * b22 + a23 * b32 + a24 * b42;
-      te[ 9 ] = a21 * b13 + a22 * b23 + a23 * b33 + a24 * b43;
-      te[ 13 ] = a21 * b14 + a22 * b24 + a23 * b34 + a24 * b44;
-
-      te[ 2 ] = a31 * b11 + a32 * b21 + a33 * b31 + a34 * b41;
-      te[ 6 ] = a31 * b12 + a32 * b22 + a33 * b32 + a34 * b42;
-      te[ 10 ] = a31 * b13 + a32 * b23 + a33 * b33 + a34 * b43;
-      te[ 14 ] = a31 * b14 + a32 * b24 + a33 * b34 + a34 * b44;
-
-      te[ 3 ] = a41 * b11 + a42 * b21 + a43 * b31 + a44 * b41;
-      te[ 7 ] = a41 * b12 + a42 * b22 + a43 * b32 + a44 * b42;
-      te[ 11 ] = a41 * b13 + a42 * b23 + a43 * b33 + a44 * b43;
-      te[ 15 ] = a41 * b14 + a42 * b24 + a43 * b34 + a44 * b44;
-
-      return *this;
-    }
-
-    static Matrix fromArray(float *elements) {
-      return Matrix(elements);
-    }
-};
-
 class Sphere {
   public:
     Vec center;
@@ -399,6 +425,18 @@ class Ray {
 
     Vec at(float t) const {
       return (this->direction * t) + this->origin;
+    }
+
+    float distanceSqToPoint(const Vec &point) const {
+      const float directionDistance = (point - this->origin).dot(this->direction);
+
+      // point behind the ray
+      if (directionDistance < 0) {
+        return this->origin.distanceToSq(point);
+      }
+
+      const Vec v = (this->direction * directionDistance) + this->origin;
+      return v.distanceToSq(point);
     }
 
     bool intersectTriangle(const Tri &tri, Vec &result) const {
@@ -456,6 +494,10 @@ class Ray {
       // Ray intersects triangle.
       result = this->at(QdN / DdN);
       return true;
+    }
+
+    bool intersectsSphere(const Sphere &sphere) const {
+      return this->distanceSqToPoint(sphere.center) <= (sphere.radius * sphere.radius);
     }
 };
 
