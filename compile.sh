283a222416efef46561aa7f9fe4756c3c7d27b33
Avaer Kazmer <a@modules.io>
2020-08-10 08:59:04 -0400

    Clean up dead parts of compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 mkdir -p bin
 emcc --std=c++11 -s WASM=1 -O3 -s TOTAL_MEMORY=67108864 \
-  util.cc FastNoise.cpp noise.cc cachedNoise.cc compose.cc march.cc tssl.cc light.cc heightfield.cc flod.cc noiser.cc cull.cc vector.cc objectize.cc \
+  util.cc FastNoise.cpp noise.cc march.cc vector.cc objectize.cc \
   --pre-js prefix.js --post-js postfix.js \
   -o bin/objectize.js
 cat prefix2.js bin/objectize.js >bin/objectize2.js
