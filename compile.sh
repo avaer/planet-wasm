cc456297fd3c146557fc0a00215e0774259773c7
Avaer Kazmer <a@modules.io>
2020-08-15 09:24:35 -0400

    Add pthreads support

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 mkdir -p bin
-emcc --std=c++11 -s WASM=1 -O3 -s TOTAL_MEMORY=67108864 \
+emcc --std=c++11 -s WASM=1 -s USE_PTHREADS=1 -O3 -s TOTAL_MEMORY=149946368 \
   FastNoise.cpp noise.cc march.cc vector.cc biomes.cc objectize.cc \
   --pre-js prefix.js --post-js postfix.js \
   -o bin/objectize.js
